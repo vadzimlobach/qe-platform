0a76b486a9f23fdadc17a074ff380d6466f3f9e0
Vadzim Lobach <tzdedqkk2l9a8f689c552zwbh15xc2@bots.bitbucket.org>
2026-05-13 21:01:50 +0100

    fix: update allure:generate script

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,9 +24,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html", { open: "never" }],
+    ["html", { outputFolder: "playwright-report", open: "never" }],
     ["json", { outputFile: "test-results/results.json" }],
-    ["allure-playwright"],
+    ["allure-playwright", { outputFolder: "allure-results" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
